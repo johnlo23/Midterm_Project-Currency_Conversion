235e41b4dbf40443e6c7d19e16a510890c6ce4ac
john <john@jlny.org>
2022-10-01 22:29:50 -0400

    Added print rates function

--- WA6_Currency_Exchange.py
+++ WA6_Currency_Exchange.py
@@ -6,6 +6,7 @@ import csv  # csv Library to load and save exchange rate data
 # Constant for rate file name
 RATE_FILE = 'conversion_rates.csv'
 
+
 # Load rates from CSV file
 def load_rates(rate_file):
     f_rate_dict = dict()
@@ -15,7 +16,7 @@ def load_rates(rate_file):
             reader = csv.DictReader(csvfile)
             for row in reader:
                 f_rate_dict[row['COUNTRY']] = {'currency': row['CURRENCY'], 'rate': row['RATE']}
-        print(f_rate_dict)
+
     # If file not found
     except FileNotFoundError:
         print()
@@ -35,31 +36,69 @@ def load_rates(rate_file):
         print()
         input("Press <enter> to quit.")
         quit()
+
     return f_rate_dict
 
 
+# Print current rates
+def print_rates(f_rate_dict):
+
+    country_list = list(f_rate_dict)
+    list_len = len(country_list)
+    half_list_len = list_len // 2
+    print('Currency Name' + " "*2 + 'Country' + " "*11 + 'Rate', end='')
+    print(" "*14, end='')
+    print('Currency Name' + " "*2 + 'Country' + " "*11 + 'Rate')
+    print('-'*42, end='')
+    print(" "*9, end='')
+    print('-'*42)
+
+    for i in range(0, half_list_len):
+        country1 = country_list[i]
+        currency1 = f_rate_dict[country1]['currency']
+        rate1 = f_rate_dict[country1]['rate']
+        print(currency1, end='')
+        print(" "*(15-len(currency1)), end='')
+        print(country1, end='')
+        print(" "*(18-len(country1)), end='')
+        print(rate1, end='')
+        if (i + half_list_len) <= list_len:
+            country2 = country_list[i + half_list_len]
+            currency2 = f_rate_dict[country2]['currency']
+            rate2 = f_rate_dict[country2]['rate']
+            print(" "*(18-len(rate1)), end='')
+            print(currency2, end='')
+            print(" "*(15-len(currency2)), end='')
+            print(country2, end='')
+            print(" "*(18-len(country2)), end='')
+            print(rate2)
+    print()
+
 # Main
 
-# Initialize rate dictionary
-rate_dict = dict()
+# Set default conversion fee
+conversion_fee = 0.01
 
-# Load rates from file
+# Load rates into dictionary from file
 rate_dict = load_rates(RATE_FILE)
 
 # intro
 print("This program will help you convert currency")
 print("")  # extra blank line
 
+# Print rates
+print_rates(rate_dict)
+
 # get input from user
 cur_name = input("Tell me the name of the currency you have for exchange: ")
 cur_amount = input("Enter the amount of " + cur_name + " you want to exchange: ")
 cur_new_name = input("Tell me the name of the currency you want to convert to: ")
-cur_exch_rate = input("Enter the exchange rate from " + cur_name + " to " + cur_new_name + " : ")
+cur_exchange_rate = input("Enter the exchange rate from " + cur_name + " to " + cur_new_name + " : ")
 
 # calculate the conversion
-cur_new_amount = float(cur_amount) / float(cur_exch_rate)
-cur_new_amount = round(cur_new_amount,2) # round to 2 digits 
+cur_new_amount = float(cur_amount) / float(cur_exchange_rate)
+cur_new_amount = round(cur_new_amount, 2)  # round to 2 digits
 
 # print the results
-print("") # extra blank line
-print("You can exchange",cur_amount,cur_name,"to",cur_new_amount,cur_new_name)
+print("")  # extra blank line
+print("You can exchange", cur_amount, cur_name, "to", cur_new_amount, cur_new_name)
